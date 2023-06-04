d48bfba2f6b585a22880c3cffd4513b2c86e4716
shadowsharda <dkpro141@gmail.com>
2023-06-04 20:28:02 +0530

    Update level1_boss_ship.gd

--- enemy_of_different_level/level1_boss_ship/level1_boss_ship.gd
+++ enemy_of_different_level/level1_boss_ship/level1_boss_ship.gd
@@ -1,11 +1,14 @@
 extends Area2D
 
 const enemy_moving_speed:float=50.0
+
+#store calculated distance
 var distance_calculation:float=0
 
+#boss health
 var health: int = 100
 # var b: String = "text"
-
+var boss_stop_moving:bool=false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,5 +17,12 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	var object:Vector2 =Vector2(0,enemy_moving_speed*delta)
-	position += object
+	if boss_stop_moving==false:
+		var object:Vector2 =Vector2(0,enemy_moving_speed*delta)
+		position += object
+	
+	distance_calculation=distance_calculation+(enemy_moving_speed*delta)
+	if distance_calculation>=100:
+		boss_stop_moving=true
+		
+	#print(distance_calculation)
