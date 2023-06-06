d6c13a10dbba201830bb02f59650172ae4197d98
shadowsharda <dkpro141@gmail.com>
2023-06-06 15:22:37 +0530

    update level_1_boss_date_6_june

--- enemy_of_different_level/level1_boss_ship/level1_boss_ship.gd
+++ enemy_of_different_level/level1_boss_ship/level1_boss_ship.gd
@@ -1,19 +1,24 @@
 extends Area2D
-
+#value can be changed from editor not avilable as export variable to save pc memory
 const enemy_moving_speed:float=50.0
+const boss_must_travelled_before_starting_sideways_movement:int=150
 
 #store calculated distance
 var distance_calculation:float=0
+
+
 
 #boss health
 var health: int = 100
 # var b: String = "text"
 var boss_stop_moving:bool=false
+var boss_start_moving_sideways:bool=false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	#starting the game loop _physics_process 
 	set_physics_process(true)
-
+	set_process(false)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
@@ -22,7 +27,7 @@ func _physics_process(delta: float) -> void:
 		position += object
 	
 	distance_calculation=distance_calculation+(enemy_moving_speed*delta)
-	if distance_calculation>=100:
+	if distance_calculation>=boss_must_travelled_before_starting_sideways_movement:
 		boss_stop_moving=true
 		
 	#print(distance_calculation)
