afa72860f209a0d4e15757847c690ca49c8b706f
shadowsharda <dkpro141@gmail.com>
2023-06-08 15:08:35 +0530

    boss_level_1_special_bullet_update1

--- enemy_of_different_level/level1_boss_ship/boss_level_1_special_bullet/boss_level_1_special_bullet.gd
+++ enemy_of_different_level/level1_boss_ship/boss_level_1_special_bullet/boss_level_1_special_bullet.gd
@@ -7,8 +7,7 @@ extends bullet_inheriting_area2d
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
